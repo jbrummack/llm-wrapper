25ce1cbcb60970bda78b8d4b035f38d918f76546
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2025-07-08 11:32:29 +0200

    Add common API Endpoints

---
+++ src/urls.rs
@@ -0,0 +1,4 @@
+pub const OPENROUTER_URL: &'static str = "https://openrouter.ai/api/v1/chat/completions";
+pub const GEMINI_URL: &'static str =
+    "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions";
+pub const OPENAI_URL: &'static str = "https://api.openai.com/v1/chat/completions";
